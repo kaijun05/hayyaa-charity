85a80c6fea1f452f3a2de6f445ff670b61f9bb8e
Jonathan <jtan13122004@gmail.com>
2025-04-09 18:02:15 +0800

    FastAPI setup (remember to install and run in virtual environment)

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+class Shipment(BaseModel):
+    description: str
+    quantity: int
+    price: float
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
