27440882e05c07cd11e32a282fd091e3b91318e8
Jonathan <jtan13122004@gmail.com>
2025-04-10 18:38:15 +0800

    Finished main component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
   import Hero from "../components/Hero.svelte";
+  import Main from "../components/Main.svelte";
 </script>
 
   <!-- Hero Section -->
    <main class=" max-w-[70rem] min-w-[50rem] mx-auto fade-in-up">
       <Hero />
+      <Main />
       
-      <!-- About us Section -->
-      <!-- <div id="about-us" class="max-w-6xl mx-auto px-6 py-32">
-        <h2 class="text-4xl font-semibold mb-8">every donation deserves a story</h2>
-        <div class="space-y-6 text-white/80 max-w-3xl">
-        <p>Our platform was born out of a mission to transform the way charity works — by bringing transparency and efficiency to a field that sorely needed change.</p>
-        <p>In a world where trust in donation systems is often questioned, we leverage blockchain technology to tokenize each donation made, allowing donors to track where their money goes, see who it helps, and feel the real difference they're made.</p>
-        <p>Through the use of smart contracts, AI-powered recipient verification, and a thoughtfully designed donor dashboard, we make giving not just simple, but meaningful.</p>
-        <p>And because we care about inclusivity, our system reaches beyond the digital divide — working hand in hand with local communities, suraus, and NGOs to ensure that even the digitally invisible receive the help they deserve. Whether you're giving RM5 or RM500, we believe you should know the impact you create. With every donation, there's a story waiting to be told.</p>
-        </div>
-        </div> -->
   </main>
         
