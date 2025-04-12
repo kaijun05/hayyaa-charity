5df0da3aac93af147762fd6127b8a76b9983a639
Jonathan <jtan13122004@gmail.com>
2025-04-12 17:57:05 +0800

    Built testimonial component

---
+++ frontend/src/components/Testimonial.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+    export let header: string = ''
+    export let subText: string = ''
+</script>
+
+<div class="grid p-4 grid-cols-3">
+    <div class="flex flex-col gap-4 bg-black/60 rounded w-[15rem]">
+        <h1 class="font-bold text-2xl">{header}</h1>
+        <p class="font-light">{subText}</p>
+    </div>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import Hero from "../components/Hero.svelte";
   import Main from "../components/Main.svelte";
+  import Testimonial from "../components/Testimonial.svelte";
 </script>
 
   <!-- Hero Section -->
    <main class=" max-w-[80rem] min-w-[50rem] mx-auto fade-in-up">
       <Hero />
       <Main />
-      
-  </main>
+      <div class="grid p-4 grid-cols-3">
+        <Testimonial header='test' subText='test subtext 123 ' />
+      </div>
+    </main>
         
